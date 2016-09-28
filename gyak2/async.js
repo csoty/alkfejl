c123da8ce1f9857a717aad8ca5e94c8066a1762e
csoty <csoty95@gmail.com>
2016-09-28 12:24:29 +0200

    fak u terminal

--- gyak2/async.js
+++ gyak2/async.js
@@ -1,4 +1,9 @@
-setTimeout(function() {
-    console.log(1);
-}, 1000);
+function waitFor(ms) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            resolve();
+        }, ms);
+    });
+}
+waitFor(1000).then(function () { console.log(1); })
 console.log(2);
